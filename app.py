93660215afe399da8ecf0ca065db0db944fe5104
julian <jurreaserna@gmail.com>
2019-11-08 17:20:45 -0500

    cambia el puerto

--- app.py
+++ app.py
@@ -367,4 +367,4 @@ def update_returns_plot(exchange, leverage, start_date, end_date):
     }
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False, port=8080)
